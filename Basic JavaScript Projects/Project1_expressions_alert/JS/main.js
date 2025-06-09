10e10b61551c4f0e9b37315307b9ea60b2ebecb1
Sagargit94 <sagarvaghasiya94@gmail.com>
2025-06-09 11:11:17 -0400

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+// This line shows a popup alert in the browser
+window.alert("Welcome to the JavaScript expressions project!");
+
+// These two lines declare string variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// Concatenate Sent1 and Sent2
+var completeSentence = Sent1 + Sent2;
+
+// This line outputs the concatenated sentence on the web page
+document.write(completeSentence);
+
+// Expression example: combine string and math
+var age = 30;
+document.write("<br>In 5 years, I will be " + (age + 5) + " years old.");
